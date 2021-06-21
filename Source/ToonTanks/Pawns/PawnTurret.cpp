0aa8de36419780e829d3af038ba9a3c4e368aeef
Nikita <33836924+NikInspirer@users.noreply.github.com>
2021-06-22 00:03:00 +0300

    PawnTurret with fire by timer is added

---
+++ Source/ToonTanks/Pawns/PawnTurret.cpp
@@ -0,0 +1,43 @@
+﻿// Copyright © 2021 Nikita Kuznetsov. All Rights Reserved.
+
+#include "PawnTurret.h"
+
+#include "PawnTank.h"
+#include "Kismet/GameplayStatics.h"
+
+APawnTurret::APawnTurret()
+{
+	FireRate = 2.0f;
+	FireRange = 500.0f;
+}
+
+void APawnTurret::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetWorld()->GetTimerManager().SetTimer(FireRateTimerHandle, this, &APawnTurret::CheckFireCondition, FireRate, true);
+	PlayerPawn = Cast<APawnTank>(UGameplayStatics::GetPlayerPawn(this, 0));
+}
+
+void APawnTurret::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+void APawnTurret::CheckFireCondition()
+{
+	if (PlayerPawn)
+	{
+		if (ReturnDistanceToPlayer() <= FireRange)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Fire Success"))
+		}
+	}
+}
+
+float APawnTurret::ReturnDistanceToPlayer() const
+{
+	float Distance = 0.0f;
+	if (PlayerPawn) { Distance = FVector::Distance(PlayerPawn->GetActorLocation(), GetActorLocation()); }
+	return Distance;
+}
---
+++ Source/ToonTanks/Pawns/PawnTurret.h
@@ -0,0 +1,40 @@
+﻿// Copyright © 2021 Nikita Kuznetsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "PawnBase.h"
+
+#include "PawnTurret.generated.h"
+
+class APawnTank;
+
+/** */
+UCLASS()
+class TOONTANKS_API APawnTurret : public APawnBase
+{
+	GENERATED_BODY()
+
+public:
+	explicit APawnTurret();
+	virtual void Tick(float DeltaSeconds) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	void CheckFireCondition();
+	float ReturnDistanceToPlayer() const;
+
+	FTimerHandle FireRateTimerHandle;
+	
+	UPROPERTY()
+	APawnTank* PlayerPawn;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+	float FireRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+	float FireRange;
+};
